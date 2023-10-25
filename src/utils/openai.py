0957afff5033e60093527c32ec3795476d51c9e8
chuanmx <chuanmx20@gmail.com>
2023-10-25 16:14:15 +0800

    handle exceptions

--- src/utils/openai.py
+++ src/utils/openai.py
@@ -38,10 +38,19 @@ class OpenAI():
         self.api_key = api_key
         self.chat = Chat(sys_prompt=sys_prompt)
     
+    def error_handler(f, *args, **kwargs):
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except Exception as e:
+                return e.args
+        return wrapper
+
     def __detect_error(self, data):
         if data.get('error', None) != None:
             raise Exception(data['error']['type'], data['error']['message'])
 
+    @error_handler
     def chat_with_gpt(self, question:str, model:str="gpt-3.5-turbo", max_tokens:int=2048, max_n:int=10, stream=False):
         conn = http.client.HTTPSConnection("api.openai.com")
         context = self.chat.to_prompt(max_n=max_n)
@@ -85,6 +94,7 @@ class OpenAI():
             self.chat.add_message(Message(role="assistant", content=ret))
             return ret
     
+    @error_handler
     def complete(self, prompt:str, model:str="text-davinci-003", max_tokens:int=2048, stop:str="", stream:bool=False):
         conn = http.client.HTTPSConnection("api.openai.com")
         payload = json.dumps({
@@ -109,6 +119,7 @@ class OpenAI():
         self.__detect_error(json_data)
         return json_data['choices'][0]['text']
 
+    @error_handler
     def edit(self, input_text:str, instruction:str, model:str="text-davinci-edit-001"):
         conn = http.client.HTTPSConnection("api.openai.com")
         payload = json.dumps({
@@ -127,6 +138,7 @@ class OpenAI():
         self.__detect_error(json_data)
         return json_data['choices'][0]['text']
     
+    @error_handler
     def text_to_image(self, prompt:str, n:int=1, size:str="1024x1024"):
         """Generate image from text prompt
 
@@ -154,6 +166,7 @@ class OpenAI():
 
         return images
     
+    @error_handler
     def embedding(self, input_text:str, model:str="text-embedding-ada-002"):
         """Get vector embedding of text
 
@@ -178,6 +191,7 @@ class OpenAI():
         self.__detect_error(json_data)
         return json_data['data'][0]['embedding']
     
+    @error_handler
     def moderation(self, input_text:str):
         """Classify text as safe or toxic
 
@@ -199,6 +213,7 @@ class OpenAI():
         self.__detect_error(json_data)
         return json_data['results']
     
+    @error_handler
     def audio_to_text(self, file_path:str, language:str="en"):
         audio_file= open(file_path, "rb")
         conn = http.client.HTTPSConnection("api.openai.com")
