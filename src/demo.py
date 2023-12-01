5daa5f10646b588f58b1d11c29a4225c87401be0
Ubuntu <1800012773@pku.edu.cn>
2023-12-01 07:35:59 +0000

    Wand Working

--- src/demo.py
+++ src/demo.py
@@ -1,11 +1,12 @@
 import json
 import asyncio
 from demo_service import DemoService
+from moobius.moobius_wand import MoobiusWand
 import aioprocessing
-def main():
+import time
+def main():    
     # For newly bound channels. It doesn't hurt to bind multiple times.
-    bind_to_channels = ['3457120e-8f05-4786-a3d4-0b53d70e6bba']
-    
+    bind_to_channels = ["3457120e-8f05-4786-a3d4-0b53d70e6bba"]
     with open("config.json", "r") as f:
         config = json.load(f)
     
@@ -13,9 +14,42 @@ def main():
         db_settings = json.load(f)
 
     service = DemoService(db_settings=db_settings, **config)
-    service.start(bind_to_channels=bind_to_channels)
-    return service
+    wand = MoobiusWand(service)
+    wand.start_background_service(bind_to_channels=bind_to_channels)
+    return wand
     
 if __name__ == "__main__":
-    service = main()
-    # service.wand.send_ping()
+    wand = main()
+    
+    async def test_async_send():
+        for i in range(5):
+            await asyncio.sleep(1)
+            msg_down_body = {
+                "channel_id": "3457120e-8f05-4786-a3d4-0b53d70e6bba",
+                "recipients": ["321e7409-e19a-4608-a623-2bae497568d0", "b42d0cb1-b97a-4c63-bbab-1d456cc26490"],
+                "subtype": "text",
+                "content": {
+                    "text": "Hello! I'm Async Async Async Ayaka! " + str(i)
+                },
+                "sender": "321e7409-e19a-4608-a623-2bae497568d0",
+                "timestamp": int(time.time() * 1000)
+            }
+            await wand.async_send("msg_down", msg_down_body)
+    
+    asyncio.run(test_async_send())
+    
+    for i in range(5):
+        time.sleep(1)
+        msg_down_body = {
+            "channel_id": "3457120e-8f05-4786-a3d4-0b53d70e6bba",
+            "recipients": ["321e7409-e19a-4608-a623-2bae497568d0", "b42d0cb1-b97a-4c63-bbab-1d456cc26490"],
+            "subtype": "text",
+            "content": {
+                "text": "Hello! I'm Ayaka! " + str(i)
+            },
+            "sender": "321e7409-e19a-4608-a623-2bae497568d0",
+            "timestamp": int(time.time() * 1000)
+        }
+        wand.send("msg_down", msg_down_body)
+        
+    
--- src/moobius/moobius_wand.py
+++ src/moobius/moobius_wand.py
@@ -1,18 +1,26 @@
 import asyncio
 from dataclasses import asdict
 from dacite import from_dict
+import uuid
+import json
+import time
+
+from multiprocessing import Process
+
 from moobius.basic._types import MessageUp, Action, FeatureCall, Copy, Payload, Character
 from moobius.basic._logging_config import logger
-class MoobiusWand:
-    def __init__(self, service, wand):
-        self.service = service
-        self.wand = wand
-        
-    # =================== on_xxx, to be override ===================
-    def on(self, payload):
-        self.wand.coro_put("RECV" + payload)
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, "RECV" + payload)
     
+class MoobiusWand:
+    def __init__(self, service):
+        self.service = service
+        
+    def _service_job(self, bind_to_channels=None):
+        asyncio.run(self.service.start(bind_to_channels))
+        
+    def start_background_service(self, bind_to_channels=None):
+        p_service = Process(target=self._service_job, args=(bind_to_channels, ))
+        p_service.start()
+        time.sleep(10)
     
     def fetch_real_characters(self, channel_id):
         data = self.service.http_api.get_channel_userlist(channel_id, self.service.service_id)
@@ -46,53 +54,25 @@ class MoobiusWand:
             payload_dict = asdict(payload_obj)
 
         payload_str = self.service._ws_payload_builder.dumps(payload_dict)
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload_str)
-        self.wand.coro_put(payload_str)
+        self.service.queue.put(payload_str)
+    
+    async def async_send(self, payload_type, payload_body):
+        if isinstance(payload_body, dict):
+            payload_dict = {
+                'type': payload_type,
+                'request_id': str(uuid.uuid4()),
+                'client_id': self.service.service_id,
+                'body': payload_body
+            }
+        else:
+            payload_obj = Payload(
+                type=payload_type,
+                request_id=str(uuid.uuid4()),
+                client_id=self.service.service_id,
+                body=payload_body
+            )
 
-    def send_service_login(self):
-        payload = self.service._ws_payload_builder.service_login(self.service.service_id, self.service.access_token)
-        logger.info(f"payload {payload}")
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
+            payload_dict = asdict(payload_obj)
 
-    def send_msg_down(self, channel_id, recipients, subtype, message_content, sender):
-        payload = self.service._ws_payload_builder.msg_down(self.service.service_id, channel_id, recipients, subtype, message_content, sender)
-        logger.info(f"msg_down payload {payload}")
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
-
-    def send_update(self, target_client_id, data):
-        payload = self.service._ws_payload_builder.update(self.service.service_id, target_client_id, data)
-        logger.info(payload)
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
-
-    def send_update_userlist(self, channel_id, user_list, recipients):
-        payload = self.service._ws_payload_builder.update_userlist(self.service.service_id, channel_id, user_list, recipients)
-        logger.info(f"send_update_userlist {payload}")
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
-
-    def send_update_channel_info(self, channel_id, channel_data):
-        payload = self.service._ws_payload_builder.update_channel_info(self.service.service_id, channel_id, channel_data)
-        logger.info(payload)
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
-
-    def send_update_playground(self, channel_id, content, recipients):
-        payload = self.service._ws_payload_builder.update_playground(self.service.service_id, channel_id, content, recipients)
-        logger.info(payload)
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
-
-    def send_update_features(self, channel_id, feature_data, recipients):
-        payload = self.service._ws_payload_builder.update_features(self.service.service_id, channel_id, feature_data, recipients)
-        logger.info(payload)
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
-
-    def send_ping(self):
-        logger.info("Sending ping...")
-        payload = self.service._ws_payload_builder.ping()
-        # self.loop.call_soon_threadsafe(self.wand.put_nowait, payload)
-        self.wand.coro_put(payload)
+        payload_str = self.service._ws_payload_builder.dumps(payload_dict)
+        await self.service.queue.coro_put(payload_str)
