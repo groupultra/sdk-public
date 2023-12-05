edb0e99378c2b537c17f1b5f07fe0e704f1755d5
jcq15 <qq842205264@gmail.com>
2023-12-05 00:10:07 +0000

    modify requirements

--- src/setup.py
+++ src/setup.py
@@ -5,4 +5,14 @@ setup(
     version='0.0.1',
     description='Moobius SDK',
     packages=['moobius'],
+    install_requires=[
+        'requests',
+        'aioprocessing',
+        'aiohttp',
+        'APScheduler',
+        'dacite',
+        'openai',
+        'redis',
+        'websockets',
+    ],
 )
