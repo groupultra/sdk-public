74e5e574bf28fdf595a7d890f21e3df429ba5962
kjkostlan <kostlankj@gmail.com>
2024-03-01 23:59:12 -0800

    TOML and liscence.md

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='moobius',
-    version='0.0.1',
+    version='1.0.0',
     description='Moobius SDK',
     packages=['moobius'],
     install_requires=[
